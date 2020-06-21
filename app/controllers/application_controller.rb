7966f70145495a670f149d6c696ad8ad33fb3d09
fernanluis <luisfer439@hotmail.com>
2020-06-21 00:18:34 -0300

    corrección en devise registration y sessions, generamos multiples layout con herencia teniendo en cuentra el registro o no registro de un usuario.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # Now the default value is to raise an exception if the token is not matched.
+  # The API calls will not have the token. If the application is expecting api calls
+  # then the strategy should be changed from :exception to :null_session.
+  protect_from_forgery with: :exception
+
+  layout :set_layout
+
+  protected
+  def set_layout
+      "application"
+  end
+
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,15 @@
 class MainController < ApplicationController
+
   def home
   end
 
   def unregistered
+  end
+
+  protected
+  def set_layout
+    return "landing" if action_name == "unregistered"
+    super
   end
 
 end
