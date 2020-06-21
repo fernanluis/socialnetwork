d54c52d87795fb15330167516638310fe35ca57e
fernanluis <luisfer439@hotmail.com>
2020-06-21 16:24:32 -0300

    Realizando pruebas unitarias con métodos de shoulda-matchers, métodos de should como validate_presence_of, etc. Parámetro: username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}" comentado para escribir pruebas
-  it "Should work" 
+  it { should validate_presence_of :username}
+  it { should validate_uniqueness_of :username}
+  it { should validate_length_of(:username).is_at_least(3).is_at_most(12)}
 end
